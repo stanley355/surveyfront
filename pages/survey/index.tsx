a00bb97dc5c65ce9974ece9cabfb9d920e1e64ec
stanley355 <winatastanley355@gmail.com>
2021-12-09 13:32:46 +0700

    Survey

---
+++ pages/survey/index.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { Field, Form, Formik } from "formik";
+import * as Yup from "yup";
+import surveyQuestions from "../api/survey-questions.json";
+
+import styles from "./survey.module.scss";
+
+import Input from "../../components/Input/Input";
+
+const Survey = () => {
+  const [loading, setLoading] = useState(false);
+
+  const surveyInitialValues = {
+    answer_1: "",
+    answer_2: "",
+    answer_3: "",
+    answer_4: "",
+    answer_5: "",
+  };
+
+  const surveySchema = Yup.object().shape({
+    answer_1: Yup.string().required("Wajib diisi"),
+    answer_2: Yup.string().required("Wajib diisi"),
+    answer_3: Yup.string().required("Wajib diisi"),
+    answer_4: Yup.string().required("Wajib diisi"),
+    answer_5: Yup.string().required("Wajib diisi"),
+  });
+
+  const questionsComponent = (question: any) => {
+    return (
+      <div>
+        <div>
+          {question.no} {question.question}{" "}
+        </div>
+        {question.answerList.map((answer: any) => (
+          <div>
+            <Field type='radio' name={question.name} value={answer} />
+            <label htmlFor={question.name}>{answer}</label>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  return (
+    <div className={styles.survey}>
+      <h1 className={styles.survey_title}>Please fill all of the following questions</h1>
+
+      <Formik
+        initialValues={surveyInitialValues}
+        validationSchema={surveySchema}
+        onSubmit={async (values) => {
+          setLoading(true);
+          // await startupsurveyHandler(values);
+          setLoading(false);
+        }}
+      >
+        {({ values }) => (
+          <Form noValidate>
+            {surveyQuestions.questionList.map((question) => questionsComponent(question))}
+
+            <button className={styles.survey_submitBtn} type='submit'>
+              {loading ? "Loading..." : "Submit"}
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default Survey;
